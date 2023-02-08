e9a1961ee3040354291cc20d1a851a0cc0a2e945
Anelizi <anelizicarvalho@gmail.com>
2023-02-07 23:50:08 -0300

    páginas componetizadas

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Corpo from "./componentes/Corpo";
+import NavBar from "./componentes/NavBar";
+import SideBar from "./componentes/SideBar";
+import Usuario from "./componentes/Usuario";
+
+export default function App() {
+    return(
+        <>
+            <Corpo/>
+            <SideBar/>
+            <NavBar/>
+            <Usuario/>
+        </>
+    )
+}
---
+++ src/componentes/Corpo.js
@@ -0,0 +1,19 @@
+import NavBar from "./NavBar";
+import Posts from "./Posts";
+import SideBar from "./SideBar";
+import Stories from "./Stories";
+
+export default function Corpo() {
+  return (
+    <>
+      <NavBar />
+      <div class="corpo">
+        <div class="esquerda">
+          <Stories />
+          <Posts />
+        </div>
+        <SideBar />
+      </div>
+    </>
+  );
+}
